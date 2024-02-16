2c82bd6c9d6014c06ff127ff0430c03f07269ed3
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-02-16 13:04:52 -0700

    Add Spring Initialization folder

---
+++ realestateapi/realestateapi/src/main/java/io/github/ejlan/realestateapi/RealestateapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.ejlan.realestateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateapiApplication.class, args);
+	}
+
+}
---
+++ realestateapi/realestateapi/src/test/java/io/github/ejlan/realestateapi/RealestateapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ejlan.realestateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
