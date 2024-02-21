3565cf16f36362bbc215a9f7d9ad6a99e8d7bb70
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-02-20 19:06:53 -0700

    Add Property Repository

---
+++ realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/repository/PropertyRepository.java
@@ -0,0 +1,28 @@
+package com.codewithej.realestateapi.repository;
+
+import com.codewithej.realestateapi.model.Property;
+import com.codewithej.realestateapi.model.PropertyType;
+import com.codewithej.realestateapi.model.PropertyStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+    List<Property> findByStatus(PropertyStatus status);
+
+    List<Property> findByType(PropertyType type);
+
+    List<Property> findByPriceBetween(Double minPrice, Double maxPrice);
+
+    List<Property> findByBedrooms(Integer bedrooms);
+
+    List<Property> findByBathroms(Integer bathrooms);
+
+    List<Property> findBySquareFootageBetween(Integer minSquareFootage, Integer maxSquareFootage);
+
+    List<Property> findByListingDateAfter(Date date);
+
+}
