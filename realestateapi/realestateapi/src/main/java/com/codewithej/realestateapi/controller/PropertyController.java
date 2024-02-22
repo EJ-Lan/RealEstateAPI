7b9ef42b2e6f408a619b874fda127d448963829b
EJ-Lan <ervinjon_lanada@outlook.com>
2024-02-22 03:05:16 -0700

    Create basic CRUD endpoints for property in controller

--- realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/controller/PropertyController.java
+++ realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/controller/PropertyController.java
@@ -1,9 +1,13 @@
 package com.codewithej.realestateapi.controller;
 
+import com.codewithej.realestateapi.dto.PropertyDTO;
 import com.codewithej.realestateapi.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/properties")
@@ -14,5 +18,35 @@ public class PropertyController {
     @Autowired
     public PropertyController(PropertyService propertyService) {
         this.propertyService = propertyService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PropertyDTO> createProperty(@RequestBody PropertyDTO propertyDTO) {
+        PropertyDTO createdProperty = propertyService.createProperty(propertyDTO);
+        return new ResponseEntity<>(createdProperty, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PropertyDTO> getPropertyById(@PathVariable Long id) {
+        PropertyDTO propertyDTO= propertyService.getPropertyById(id);
+        return ResponseEntity.ok(propertyDTO);
+    }
+
+    @PutMapping("/{id")
+    public ResponseEntity<PropertyDTO> updateProperty(@PathVariable Long id, @RequestBody PropertyDTO propertyDTO) {
+        PropertyDTO updatedProperty = propertyService.updateProperty(id, propertyDTO);
+        return ResponseEntity.ok(updatedProperty);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProperty(@PathVariable Long id) {
+        propertyService.deleteProperty(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PropertyDTO>> getAllProperties() {
+        List<PropertyDTO> properties = propertyService.getAllProperties();
+        return ResponseEntity.ok(properties);
     }
 }
