9c8f522fed61f7eb8bc0b71421696a90b876d31c
EJ-Lan <ervinjon_lanada@outlook.com>
2024-02-22 02:54:30 -0700

    Create the Initial Property Controller Class

---
+++ realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/controller/PropertyController.java
@@ -0,0 +1,18 @@
+package com.codewithej.realestateapi.controller;
+
+import com.codewithej.realestateapi.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/properties")
+public class PropertyController {
+
+    private final PropertyService propertyService;
+
+    @Autowired
+    public PropertyController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+}
