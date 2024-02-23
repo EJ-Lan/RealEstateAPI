1c863b963b7620bf250b2faa6fd13885db2d60d4
EJ-Lan <ervinjon_lanada@outlook.com>
2024-02-23 01:49:18 -0700

    Create New Endpoints in PropertyController

--- realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/controller/PropertyController.java
+++ realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/controller/PropertyController.java
@@ -1,12 +1,16 @@
 package com.codewithej.realestateapi.controller;
 
 import com.codewithej.realestateapi.dto.PropertyDTO;
+import com.codewithej.realestateapi.model.PropertyStatus;
+import com.codewithej.realestateapi.model.PropertyType;
 import com.codewithej.realestateapi.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -49,4 +53,48 @@ public class PropertyController {
         List<PropertyDTO> properties = propertyService.getAllProperties();
         return ResponseEntity.ok(properties);
     }
+
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<PropertyDTO>> findByStatus(@PathVariable PropertyStatus status) {
+        List<PropertyDTO> properties = propertyService.findByStatus(status);
+        return ResponseEntity.ok(properties);
+    }
+
+    @GetMapping("/type/{type}")
+    public ResponseEntity<List<PropertyDTO>> findByType(@PathVariable PropertyType type) {
+        List<PropertyDTO> properties = propertyService.findByType(type);
+        return ResponseEntity.ok(properties);
+    }
+
+    @GetMapping("/bedrooms/{bedrooms}")
+    public ResponseEntity<List<PropertyDTO>> findByBedrooms(@PathVariable Integer bedrooms) {
+        List<PropertyDTO> properties = propertyService.findByBedrooms(bedrooms);
+        return ResponseEntity.ok(properties);
+    }
+
+    @GetMapping("/bathrooms/{bathrooms}")
+    public ResponseEntity<List<PropertyDTO>> findByBathrooms(@PathVariable Integer bathrooms) {
+        List<PropertyDTO> properties = propertyService.findByBathrooms(bathrooms);
+        return ResponseEntity.ok(properties);
+    }
+
+    @GetMapping("/price")
+    public ResponseEntity<List<PropertyDTO>> findByPriceRange(@RequestParam Double minPrice, @RequestParam Double maxPrice) {
+        List<PropertyDTO> properties = propertyService.findByPriceRange(minPrice, maxPrice);
+        return ResponseEntity.ok(properties);
+    }
+
+    @GetMapping("/squareFootage")
+    public ResponseEntity<List<PropertyDTO>> findBySquareFootageRange(@RequestParam Integer minSquareFootage, @RequestParam Integer maxSquareFootage) {
+        List<PropertyDTO> properties = propertyService.findBySquareFootageRange(minSquareFootage, maxSquareFootage);
+        return ResponseEntity.ok(properties);
+    }
+
+    @GetMapping("/listingDateAfter")
+    public ResponseEntity<List<PropertyDTO>> findByListingDateAfter(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date) {
+        List<PropertyDTO> properties = propertyService.findByListingDateAfter(date);
+        return ResponseEntity.ok(properties);
+    }
+
+
 }
