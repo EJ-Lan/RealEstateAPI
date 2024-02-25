290c8932db7ee4618a4eb6f48c42021262582ff2
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-02-25 14:30:50 -0700

    Change application properties

--- realestateapi/realestateapi/src/main/java/com/codewithej/realestateapi/config/SecretsConfig.java
+++
@@ -1,9 +0,0 @@
-package com.codewithej.realestateapi.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:secrets.properties")
-public class SecretsConfig {
-}
