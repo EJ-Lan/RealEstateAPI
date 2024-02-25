d4b6c2b3301780e5b1b4f7437a4c0e5e98909777
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-02-24 17:53:57 -0700

    Client Controller Tests

--- realestateapi/realestateapi/src/test/java/com/codewithej/realestateapi/controller/PropertyControllerTest.java
+++ realestateapi/realestateapi/src/test/java/com/codewithej/realestateapi/controller/PropertyControllerTest.java
@@ -8,11 +8,13 @@ import com.codewithej.realestateapi.service.PropertyService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.text.SimpleDateFormat;
@@ -28,6 +30,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(PropertyController.class)
 @Import(TestSecurityConfig.class)
 public class PropertyControllerTest {
